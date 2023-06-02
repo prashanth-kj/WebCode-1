2ead4bf7f18902cb467688eaee57e8ed7ab1ec57
prashanth-kj <129944223+prashanth-kj@users.noreply.github.com>
2023-06-02 19:44:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+let container = document.querySelector('.container');
+
+function showTable(data) {
+  let tableBody = document.querySelector("#mytable tbody");
+  tableBody.innerHTML = '';
+
+  data.forEach((item) => {
+    let row = document.createElement('tr');
+    row.innerHTML = `
+
+      <td>${item.name}</td>
+      <td>${item.brewery_type}</td>
+      <td>${item.address_1}</td>
+      <td>${item.phone}</td>
+      <td><a href="${item.website_url}" target="_blank">click link</a></td>
+     `;
+    tableBody.appendChild(row);
+  });
+}
+
+function getDetails(data) {
+  let row = document.createElement('div');
+  row.classList.add('row');
+
+  for (let i = 0; i < data.length; i++) {
+    let col = document.createElement('div');
+    col.classList.add('col-md-4');
+    let card = `
+      <div class="card border-dark mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Brewery Details:</h5>
+          <p class="card-text">Name: ${data[i].name}</p>
+          <p class="card-text">Brewery Type: ${data[i].brewery_type}</p>
+          <p class="card-text">Address: ${data[i].address_1}</p>
+          <p class="card-text">Phone: ${data[i].phone}</p>
+          <a href="${data[i].website_url}" class="btn btn-primary" target="_blank">Click URL</a>
+        </div>
+      </div>
+    `;
+    col.innerHTML = card;
+    row.appendChild(col);
+  }
+
+  container.appendChild(row);
+}
+
+async function fetchData() {
+  try {
+    let response = await fetch(`https://api.openbrewerydb.org/v1/breweries`);
+    let data = await response.json();
+    getDetails(data);
+    showTable(data);
+   
+  } catch (error) {
+    console.log("Error fetching the data:", error);
+  }
+}
+
+fetchData();
